7d4fb7d2ecd3b54e3c2c5e1f10016ff2e75be0e8
chiou8511 <chiou8511829@gmail.com>
2024-06-11 14:06:52 +0800

    01
    01

---
+++ Assets/c#/playermove.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class playermove : MonoBehaviour
+{
+    private Rigidbody2D rb2;
+    private SpriteRenderer sp;
+    public static int trafficlight_touch = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb2 != null)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                rb2.velocity = new Vector2(-5, rb2.velocity.y);
+                sp.flipX = true;
+            }
+           
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                rb2.velocity = new Vector2(5, rb2.velocity.y);
+                sp.flipX = false;
+            }
+            
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rb2.velocity = new Vector2(rb2.velocity.x, 5);
+            }
+           
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                rb2.velocity = new Vector2(rb2.velocity.x, -5);
+            }
+            
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "goal")
+        {
+           
+        }
+       
+    }
+}
